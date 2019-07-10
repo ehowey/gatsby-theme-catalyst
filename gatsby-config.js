d11eff1aafed1d4c681ee93d8cb412838524f7d5
Eric Howey <ehowey@gmail.com>
2019-07-09 22:47:23 -0600

    Updated remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,17 @@ module.exports = {
         defaultLayouts: {
           default: require.resolve(`./src/components/layout`),
         },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1380,
+              linkImagesToOriginal: false,
+            },
+          },
+          { resolve: `gatsby-remark-copy-linked-files` },
+          { resolve: `gatsby-remark-smartypants` },
+        ],
       },
     },
     `gatsby-plugin-react-helmet`,
