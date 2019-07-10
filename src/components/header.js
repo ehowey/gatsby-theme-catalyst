6a7d84134b68b60e009b13914898411dbf474964
Eric Howey <ehowey@gmail.com>
2019-07-09 23:44:19 -0600

    Updated header

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,8 @@ const siteHeader = props => {
         sx={{
           display: "grid",
           gridTemplateColumns: "auto 1fr",
-          position: theme => theme.headerPosition,
+          width: "100%",
+          position: theme => theme.catalystOptions.headerPosition,
           color: props.open ? "header.textOpen" : "header.text",
           backgroundColor: props.open ? "header.backgroundOpen" : "header.background",
           padding: "0.5rem 0",
