e134c7233d91ecce295cedbb32a59e7492a78fcc
Eric Howey <ehowey@gmail.com>
2019-07-27 23:11:54 -0600

    Social component export

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-export { default as Layout } from './src/components/layout';
-export { default as SEO } from './src/components/seo';
-export { default as baseTheme } from './src/gatsby-plugin-theme-ui';
+export { default as Layout } from "./src/components/layout"
+export { default as SEO } from "./src/components/seo"
+export { default as Social } from "./src/components/social"
+export { default as baseTheme } from "./src/gatsby-plugin-theme-ui"
