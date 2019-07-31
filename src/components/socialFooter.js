f4fc761d040958c203a01f52606c63d11fd69d62
Eric Howey <ehowey@gmail.com>
2019-07-30 20:46:30 -0600

    Added more social icons and links

--- src/components/socialFooter.js
+++ src/components/socialFooter.js
@@ -10,6 +10,29 @@ import {
   FaYoutube,
   FaInstagram,
   FaLinkedinIn,
+  FaBehance,
+  FaBitbucket,
+  FaDribbble,
+  Fa500Px,
+  FaEtsy,
+  FaFlickr,
+  FaGitlab,
+  FaLastfm,
+  FaMediumM,
+  FaPatreon,
+  FaPaypal,
+  FaPinterestP,
+  FaRedditAlien,
+  FaSlack,
+  FaSnapchatGhost,
+  FaSoundcloud,
+  FaSpotify,
+  FaTumblr,
+  FaWeibo,
+  FaVimeoV,
+  FaYelp,
+  FaQq,
+  FaWhatsapp,
 } from "react-icons/fa"
 
 const siteSocial = () => {
@@ -58,7 +81,7 @@ const siteSocial = () => {
             </a>
           )
         } else if (
-          socialName === "github" &&
+          socialName === "500px" &&
           (socialLocation === "footer" || socialLocation === "all")
         ) {
           return (
@@ -68,7 +91,77 @@ const siteSocial = () => {
               rel="noopener noreferrer"
               key={platform.name}
             >
-              <FaGithub />
+              <Fa500Px />
+            </a>
+          )
+        } else if (
+          socialName === "behance" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaBehance />
+            </a>
+          )
+        } else if (
+          socialName === "bitbucket" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaBitbucket />
+            </a>
+          )
+        } else if (
+          socialName === "dribble" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaDribbble />
+            </a>
+          )
+        } else if (
+          socialName === "etsy" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaEtsy />
+            </a>
+          )
+        } else if (
+          socialName === "flickr" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaFlickr />
             </a>
           )
         } else if (
@@ -86,7 +179,7 @@ const siteSocial = () => {
             </a>
           )
         } else if (
-          socialName === "youtube" &&
+          socialName === "github" &&
           (socialLocation === "footer" || socialLocation === "all")
         ) {
           return (
@@ -96,7 +189,21 @@ const siteSocial = () => {
               rel="noopener noreferrer"
               key={platform.name}
             >
-              <FaYoutube />
+              <FaGithub />
+            </a>
+          )
+        } else if (
+          socialName === "gitlab" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaGitlab />
             </a>
           )
         } else if (
@@ -114,6 +221,20 @@ const siteSocial = () => {
             </a>
           )
         } else if (
+          socialName === "lastfm" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaLastfm />
+            </a>
+          )
+        } else if (
           socialName === "linkedin" &&
           (socialLocation === "footer" || socialLocation === "all")
         ) {
@@ -125,6 +246,230 @@ const siteSocial = () => {
               key={platform.name}
             >
               <FaLinkedinIn />
+            </a>
+          )
+        } else if (
+          socialName === "medium" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaMediumM />
+            </a>
+          )
+        } else if (
+          socialName === "patreon" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaPatreon />
+            </a>
+          )
+        } else if (
+          socialName === "paypal" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaPaypal />
+            </a>
+          )
+        } else if (
+          socialName === "pinterest" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaPinterestP />
+            </a>
+          )
+        } else if (
+          socialName === "reddit" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaRedditAlien />
+            </a>
+          )
+        } else if (
+          socialName === "slack" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSlack />
+            </a>
+          )
+        } else if (
+          socialName === "snapchat" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSnapchatGhost />
+            </a>
+          )
+        } else if (
+          socialName === "soundcloud" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSoundcloud />
+            </a>
+          )
+        } else if (
+          socialName === "spotify" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSpotify />
+            </a>
+          )
+        } else if (
+          socialName === "tumblr" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaTumblr />
+            </a>
+          )
+        } else if (
+          socialName === "weibo" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaWeibo />
+            </a>
+          )
+        } else if (
+          socialName === "vimeo" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaVimeoV />
+            </a>
+          )
+        } else if (
+          socialName === "yelp" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaYelp />
+            </a>
+          )
+        } else if (
+          socialName === "youtube" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaYoutube />
+            </a>
+          )
+        } else if (
+          socialName === "qq" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaQq />
+            </a>
+          )
+        } else if (
+          socialName === "whatsapp" &&
+          (socialLocation === "footer" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaWhatsapp />
             </a>
           )
         } else if (socialLocation === "footer" || socialLocation === "all") {
--- src/components/socialHeader.js
+++ src/components/socialHeader.js
@@ -10,6 +10,29 @@ import {
   FaYoutube,
   FaInstagram,
   FaLinkedinIn,
+  FaBehance,
+  FaBitbucket,
+  FaDribbble,
+  Fa500Px,
+  FaEtsy,
+  FaFlickr,
+  FaGitlab,
+  FaLastfm,
+  FaMediumM,
+  FaPatreon,
+  FaPaypal,
+  FaPinterestP,
+  FaRedditAlien,
+  FaSlack,
+  FaSnapchatGhost,
+  FaSoundcloud,
+  FaSpotify,
+  FaTumblr,
+  FaWeibo,
+  FaVimeoV,
+  FaYelp,
+  FaQq,
+  FaWhatsapp,
 } from "react-icons/fa"
 
 const siteSocial = () => {
@@ -29,7 +52,7 @@ const siteSocial = () => {
   `)
   const { theme } = useThemeUI()
   return (
-    <IconContext.Provider value={{ size: theme.sizes.iconsHeader }}>
+    <IconContext.Provider value={{ size: theme.sizes.iconsheader }}>
       {data.site.siteMetadata.socialLinks.map(platform => {
         let socialName = platform.name.toLowerCase()
         let socialLocation = platform.location.toLowerCase()
@@ -58,7 +81,7 @@ const siteSocial = () => {
             </a>
           )
         } else if (
-          socialName === "github" &&
+          socialName === "500px" &&
           (socialLocation === "header" || socialLocation === "all")
         ) {
           return (
@@ -68,7 +91,77 @@ const siteSocial = () => {
               rel="noopener noreferrer"
               key={platform.name}
             >
-              <FaGithub />
+              <Fa500Px />
+            </a>
+          )
+        } else if (
+          socialName === "behance" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaBehance />
+            </a>
+          )
+        } else if (
+          socialName === "bitbucket" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaBitbucket />
+            </a>
+          )
+        } else if (
+          socialName === "dribble" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaDribbble />
+            </a>
+          )
+        } else if (
+          socialName === "etsy" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaEtsy />
+            </a>
+          )
+        } else if (
+          socialName === "flickr" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaFlickr />
             </a>
           )
         } else if (
@@ -86,7 +179,7 @@ const siteSocial = () => {
             </a>
           )
         } else if (
-          socialName === "youtube" &&
+          socialName === "github" &&
           (socialLocation === "header" || socialLocation === "all")
         ) {
           return (
@@ -96,7 +189,21 @@ const siteSocial = () => {
               rel="noopener noreferrer"
               key={platform.name}
             >
-              <FaYoutube />
+              <FaGithub />
+            </a>
+          )
+        } else if (
+          socialName === "gitlab" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaGitlab />
             </a>
           )
         } else if (
@@ -114,6 +221,20 @@ const siteSocial = () => {
             </a>
           )
         } else if (
+          socialName === "lastfm" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaLastfm />
+            </a>
+          )
+        } else if (
           socialName === "linkedin" &&
           (socialLocation === "header" || socialLocation === "all")
         ) {
@@ -125,6 +246,230 @@ const siteSocial = () => {
               key={platform.name}
             >
               <FaLinkedinIn />
+            </a>
+          )
+        } else if (
+          socialName === "medium" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaMediumM />
+            </a>
+          )
+        } else if (
+          socialName === "patreon" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaPatreon />
+            </a>
+          )
+        } else if (
+          socialName === "paypal" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaPaypal />
+            </a>
+          )
+        } else if (
+          socialName === "pinterest" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaPinterestP />
+            </a>
+          )
+        } else if (
+          socialName === "reddit" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaRedditAlien />
+            </a>
+          )
+        } else if (
+          socialName === "slack" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSlack />
+            </a>
+          )
+        } else if (
+          socialName === "snapchat" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSnapchatGhost />
+            </a>
+          )
+        } else if (
+          socialName === "soundcloud" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSoundcloud />
+            </a>
+          )
+        } else if (
+          socialName === "spotify" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaSpotify />
+            </a>
+          )
+        } else if (
+          socialName === "tumblr" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaTumblr />
+            </a>
+          )
+        } else if (
+          socialName === "weibo" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaWeibo />
+            </a>
+          )
+        } else if (
+          socialName === "vimeo" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaVimeoV />
+            </a>
+          )
+        } else if (
+          socialName === "yelp" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaYelp />
+            </a>
+          )
+        } else if (
+          socialName === "youtube" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaYoutube />
+            </a>
+          )
+        } else if (
+          socialName === "qq" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaQq />
+            </a>
+          )
+        } else if (
+          socialName === "whatsapp" &&
+          (socialLocation === "header" || socialLocation === "all")
+        ) {
+          return (
+            <a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={platform.name}
+            >
+              <FaWhatsapp />
             </a>
           )
         } else if (socialLocation === "header" || socialLocation === "all") {
