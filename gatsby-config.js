306fd554f1cbaee89ea196c71ab433ddabc800c1
Eric Howey <ehowey@gmail.com>
2019-08-25 20:30:19 -0600

    Allow options to be passed, remove siteMetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,47 @@
-module.exports = {
-  siteMetadata: {
-    title: `Catalyst Core`,
-    description: `Speed up your Gatsby development workflow. Designed as an opinionated and high speed base theme.`,
-    author: `Eric Howey`,
-    siteUrl: `https://www.example.com`,
-  },
-  plugins: [
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.md`, `.mdx`],
-        defaultLayouts: {
-          default: require.resolve(`./src/components/layout`),
+module.exports = options => {
+  return {
+    plugins: [
+      {
+        resolve: `gatsby-plugin-mdx`,
+        options: {
+          extensions: [`.md`, `.mdx`],
+          defaultLayouts: {
+            default: require.resolve(`./src/components/layout`),
+          },
+          gatsbyRemarkPlugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 1380,
+                linkImagesToOriginal: false,
+                withWebp: true,
+              },
+            },
+            {
+              resolve: `gatsby-remark-copy-linked-files`,
+              options: {
+                destinationDir: `assets/images`,
+              },
+            },
+            { resolve: `gatsby-remark-smartypants` },
+          ],
+          plugins: [`gatsby-remark-images`],
         },
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1380,
-              linkImagesToOriginal: false,
-              withWebp: true,
-            },
-          },
-          {
-            resolve: `gatsby-remark-copy-linked-files`,
-            options: {
-              destinationDir: `assets/images`,
-            },
-          },
-          { resolve: `gatsby-remark-smartypants` },
-        ],
-        plugins: [`gatsby-remark-images`],
       },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `src/`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `src`,
+          path: `src/`,
+        },
       },
-    },
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-theme-ui`,
-    `gatsby-plugin-offline`,
-  ],
+      `gatsby-plugin-sitemap`,
+      `gatsby-plugin-robots-txt`,
+      `gatsby-plugin-react-helmet`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-theme-ui`,
+      `gatsby-plugin-offline`,
+    ],
+  }
 }
