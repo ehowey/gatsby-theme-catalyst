9cb09dfc94c8a117a88f7990fb8b47c27d1a6158
Eric Howey <ehowey@gmail.com>
2019-09-20 22:49:22 -0600

    Updated exports

--- index.js
+++ index.js
@@ -7,7 +7,26 @@ export {
 export {
   default as SocialHeaderIcons,
 } from "./src/components/social/socialHeaderIcons"
-export { default as BaseTheme } from "./src/gatsby-plugin-theme-ui"
+export {
+  default as BaseThemeBreakpoints,
+} from "./src/gatsby-plugin-theme-ui/breakpoints"
+export { default as BaseThemeColors } from "./src/gatsby-plugin-theme-ui/colors"
+export { default as BaseThemeFonts } from "./src/gatsby-plugin-theme-ui/fonts"
+export {
+  default as BaseThemeFontSizes,
+} from "./src/gatsby-plugin-theme-ui/fontSizes"
+export {
+  default as BaseThemeFontWeights,
+} from "./src/gatsby-plugin-theme-ui/fontWeights"
+export {
+  default as BaseThemeLineHeights,
+} from "./src/gatsby-plugin-theme-ui/lineHeights"
+export { default as BaseThemeSizes } from "./src/gatsby-plugin-theme-ui/sizes"
+export { default as BaseThemeSpaces } from "./src/gatsby-plugin-theme-ui/spaces"
+export { default as BaseThemeStyles } from "./src/gatsby-plugin-theme-ui/styles"
+export {
+  default as BaseThemeVariants,
+} from "./src/gatsby-plugin-theme-ui/variants"
 export {
   default as ButtonInternal,
 } from "./src/components/design-system/buttonInternal"
