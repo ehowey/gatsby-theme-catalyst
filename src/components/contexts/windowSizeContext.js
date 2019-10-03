c07086078017d43a2a5b8af7132fbd536cb1ef4e
Eric Howey <ehowey@gmail.com>
2019-10-02 22:23:00 -0600

    Additional fix to window size hook

--- src/components/contexts/windowSizeContext.js
+++ src/components/contexts/windowSizeContext.js
@@ -1,33 +1,39 @@
 import React, { createContext, useContext, useState, useEffect } from "react"
 import debounce from "lodash.debounce"
 
+let defaultHeight
+let defaultWidth
+
+if (typeof window !== `undefined`) {
+  defaultHeight = window.innerHeight
+  defaultWidth = window.innerWidth
+}
+
 const WindowSizeContext = createContext(null)
 
 export const WindowSizeProvider = ({ children }) => {
-  if (typeof window !== "undefined") {
-    const [size, setSize] = useState({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    })
-    useEffect(() => {
-      const handleResize = () => {
-        setSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        })
-      }
-      window.addEventListener("resize", debounce(handleResize, 300))
-      return () => {
-        window.removeEventListener("resize", debounce(handleResize, 300))
-      }
-    }, [])
+  const [size, setSize] = useState({
+    width: defaultWidth,
+    height: defaultHeight,
+  })
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+    window.addEventListener("resize", debounce(handleResize, 300))
+    return () => {
+      window.removeEventListener("resize", debounce(handleResize, 300))
+    }
+  }, [])
 
-    return (
-      <WindowSizeContext.Provider value={size}>
-        {children}
-      </WindowSizeContext.Provider>
-    )
-  }
+  return (
+    <WindowSizeContext.Provider value={size}>
+      {children}
+    </WindowSizeContext.Provider>
+  )
 }
 
 export const useWindowSize = () => useContext(WindowSizeContext)
