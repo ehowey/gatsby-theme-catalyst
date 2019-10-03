8647e4da747e5a624b59d92ba717d6867cea6fac
Eric Howey <ehowey@gmail.com>
2019-10-02 22:04:35 -0600

    0.62 - Bug fix undefined window

--- src/components/contexts/windowSizeContext.js
+++ src/components/contexts/windowSizeContext.js
@@ -4,27 +4,30 @@ import debounce from "lodash.debounce"
 const WindowSizeContext = createContext(null)
 
 export const WindowSizeProvider = ({ children }) => {
-  const [size, setSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  })
-  useEffect(() => {
-    const handleResize = () => {
-      setSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      })
-    }
-    window.addEventListener("resize", debounce(handleResize, 300))
-    return () => {
-      window.removeEventListener("resize", debounce(handleResize, 300))
-    }
-  }, [])
-  return (
-    <WindowSizeContext.Provider value={size}>
-      {children}
-    </WindowSizeContext.Provider>
-  )
+  if (typeof window !== "undefined") {
+    const [size, setSize] = useState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    })
+    useEffect(() => {
+      const handleResize = () => {
+        setSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        })
+      }
+      window.addEventListener("resize", debounce(handleResize, 300))
+      return () => {
+        window.removeEventListener("resize", debounce(handleResize, 300))
+      }
+    }, [])
+
+    return (
+      <WindowSizeContext.Provider value={size}>
+        {children}
+      </WindowSizeContext.Provider>
+    )
+  }
 }
 
 export const useWindowSize = () => useContext(WindowSizeContext)
