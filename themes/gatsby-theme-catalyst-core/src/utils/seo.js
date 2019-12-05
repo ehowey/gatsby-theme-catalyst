54da7b790ac661083dee30ea46e28becae118364
Eric Howey <ehowey@gmail.com>
2019-12-04 22:24:05 -0700

    Updated SEO component, adding image support

--- themes/gatsby-theme-catalyst-core/src/utils/seo.js
+++ themes/gatsby-theme-catalyst-core/src/utils/seo.js
@@ -1,99 +1,155 @@
-import React from "react"
-import Helmet from "react-helmet"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
 
-function SEO({ description, meta, image: metaImage, title }) {
-  return (
-    <StaticQuery
-      query={graphql`
-        {
-          site {
-            siteMetadata {
-              author
-              description
-              siteUrl
-              keywords
-            }
-          }
-          defaultImage: allFile(
-            filter: { relativePath: { eq: "logo-512.png" } }
-          ) {
-            edges {
-              node {
-                childImageSharp {
-                  resize(width: 512) {
-                    src
-                    height
-                    width
-                  }
-                }
-              }
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+import { useSiteMetadata } from "./use-site-metadata"
+
+const SEO = ({
+  description: propDescription,
+  lang,
+  meta,
+  keywords: propKeywords,
+  title: propTitle,
+  image: propImage,
+  isBlogPost,
+}) => {
+  const { title, description, keywords, twitter, siteUrl } = useSiteMetadata()
+  const data = useStaticQuery(
+    graphql`
+      query {
+        # site {
+        #   siteMetadata {
+        #     title
+        #     description
+        #     keywords
+        #     author
+        #     siteUrl
+        #     twitter
+        #   }
+        # }
+        seoImage: file(relativePath: { eq: "seo-default.png" }) {
+          childImageSharp {
+            resize(width: 512) {
+              src
+              width
+              height
             }
           }
         }
-      `}
-      render={data => {
-        const metaDescription =
-          description || data.site.siteMetadata.description
-        const image =
-          metaImage && metaImage.src
-            ? `${data.site.siteMetadata.siteUrl}${metaImage.src}`
-            : null || `${data.site.siteMetadata.siteUrl}${metaImage.src}`
-        return (
-          <Helmet
-            htmlAttributes={{
-              lang: "en",
-            }}
-            title={title}
-            meta={[
-              {
-                name: "description",
-                content: metaDescription,
-              },
-              {
-                name: "keywords",
-                content: data.site.siteMetadata.keywords.join(","),
-              },
-              { property: "og:title", content: title },
-              { property: "og:description", content: metaDescription },
-              {
-                name: "twitter:creator",
-                content: data.site.siteMetadata.author,
-              },
-              { name: "twitter:title", content: title },
-              { name: "twitter:description", content: metaDescription },
-            ]
-              .concat(
-                metaImage
-                  ? [
-                      { property: "og:image", content: image },
-                      { property: "og:image:width", content: metaImage.width },
-                      {
-                        property: "og:image:height",
-                        content: metaImage.height,
-                      },
-                      { name: "twitter:card", content: "summary_large_image" },
-                    ]
-                  : [{ name: "twitter:card", content: "summary" }]
-              )
-              .concat(meta)}
-          />
-        )
+      }
+    `
+  )
+
+  const seoTitle = propTitle || title
+  const seoDescription = propDescription || description
+  const seoKeywords = propKeywords || keywords
+  const seoImage = propImage || data.seoImage.childImageSharp.resize
+  const seoImageSrc = `${siteUrl}${seoImage.src}`
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
       }}
+      title={seoTitle}
+      titleTemplate={`%s | ${title}`}
+      meta={[
+        {
+          name: `description`,
+          content: seoDescription,
+        },
+        {
+          name: `image`,
+          content: seoImageSrc,
+        },
+
+        //Open Graph
+        {
+          property: `og:title`,
+          content: seoTitle,
+        },
+        {
+          property: `og:description`,
+          content: seoDescription,
+        },
+        {
+          property: `og:type`,
+          content: isBlogPost ? `article` : `website`,
+        },
+        {
+          property: `og:image`,
+          content: seoImageSrc,
+        },
+        {
+          property: `og:image:alt`,
+          content: seoTitle,
+        },
+        { property: `og:image:width`, content: seoImage.width },
+        { property: `og:image:height`, content: seoImage.height },
+
+        // Twitter
+        {
+          name: `twitter:title`,
+          content: seoTitle,
+        },
+        {
+          name: `twitter:description`,
+          content: seoDescription,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`,
+        },
+        {
+          name: `twitter:creator`,
+          content: twitter,
+        },
+        {
+          name: `twitter:site`,
+          content: twitter,
+        },
+        {
+          name: `twitter:image`,
+          content: seoImageSrc,
+        },
+        {
+          name: `twitter:image:alt`,
+          content: seoTitle,
+        },
+      ]
+        .concat(
+          seoKeywords.length > 0
+            ? {
+                name: `keywords`,
+                content: seoKeywords.join(`, `),
+              }
+            : []
+        )
+        .concat(meta)}
     />
   )
 }
 
-SEO.defaultProps = { meta: [] }
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  isBlogPost: false,
+}
+
 SEO.propTypes = {
   description: PropTypes.string,
-  image: PropTypes.shape({
-    src: PropTypes.string.isRequired(),
-    height: PropTypes.string.isRequired(),
-    width: PropTypes.string.isRequired(),
-  }),
+  lang: PropTypes.string,
   meta: PropTypes.array,
+  keywords: PropTypes.arrayOf(PropTypes.string),
   title: PropTypes.string.isRequired,
+  isBlogPost: PropTypes.bool,
 }
+
 export default SEO
