ffca2f3b4dd44a21c6eb3ccb0a41e4c8b6c07b62
Eric Howey <ehowey@gmail.com>
2019-12-05 06:53:25 -0700

    Implement MDXProvider for SEO component

--- themes/gatsby-theme-catalyst-core/gatsby-browser.js
+++ themes/gatsby-theme-catalyst-core/gatsby-browser.js
@@ -2,13 +2,17 @@ import React from "react"
 import { NavProvider } from "./src/contexts/nav-context"
 import { MobileProvider } from "./src/contexts/mobile-context"
 import { HomeProvider } from "./src/contexts/home-context"
+import { MDXProvider } from "@mdx-js/react"
+import SEO from "./src/utils/seo"
 import Layout from "./src/components/layout"
 import NormalizeCSS from "./src/utils/normalize-css"
 
 export const wrapRootElement = ({ element }) => {
   return (
     <HomeProvider>
-      <NavProvider>{element}</NavProvider>
+      <NavProvider>
+        <MDXProvider components={{ SEO }}>{element}</MDXProvider>
+      </NavProvider>
     </HomeProvider>
   )
 }
