1e8ec6b4cd576b4c99777d3228770240047e7acd
Eric Howey <ehowey@gmail.com>
2019-12-07 06:25:31 -0700

    Remove docs site in favor of github

--- www/gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: "Gatsby Theme Catalyst Documentation",
-    description:
-      "Accelerate your Gatsby development with Gatsby Theme Catalyst!"
-  },
-  plugins: ["gatsby-theme-documentation"]
-};
