1984c322e3f7f2a6a8bda9eacf97c191878ad10d
Eric Howey <ehowey@gmail.com>
2020-01-05 22:27:11 -0700

    Fix gatsby-remark-images config

--- themes/gatsby-theme-catalyst-core/gatsby-config.js
+++ themes/gatsby-theme-catalyst-core/gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = options => {
                 maxWidth: 1440,
                 linkImagesToOriginal: false,
                 withWebp: true,
-                backgroundColor: `transparent`,
+                backgroundColor: "transparent",
               },
             },
             {
@@ -69,7 +69,17 @@ module.exports = options => {
             { resolve: `gatsby-remark-reading-time` },
           ],
           remarkPlugins: [remarkSlug],
-          plugins: [`gatsby-remark-images`],
+          plugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 1440,
+                linkImagesToOriginal: false,
+                withWebp: true,
+                backgroundColor: "transparent",
+              },
+            },
+          ],
         },
       },
       `gatsby-plugin-sitemap`,
