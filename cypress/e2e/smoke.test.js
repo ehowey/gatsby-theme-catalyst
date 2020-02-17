6493853b3e8e8a02115fdb127b93c1b7407638e6
Eric Howey <ehowey@gmail.com>
2020-02-16 22:22:56 -0700

    Improved smoke test

--- cypress/e2e/smoke.test.js
+++ cypress/e2e/smoke.test.js
@@ -1,5 +1,8 @@
 describe("Smoke test", () => {
   it("Site loads", () => {
     cy.visit(`/`).assertRoute(`/`)
+    cy.get("header").should("be.visible")
+    cy.get("main").should("be.visible")
+    cy.get("footer").should("be.visible")
   })
 })
