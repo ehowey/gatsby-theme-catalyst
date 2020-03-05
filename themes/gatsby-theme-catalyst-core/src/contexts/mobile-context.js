2bffa5819489d29484d32cc8d1de556ed5d2570d
Eric Howey <ehowey@gmail.com>
2020-03-04 21:25:57 -0700

    Remove window checks when inside useEffect

--- themes/gatsby-theme-catalyst-core/src/contexts/mobile-context.js
+++ themes/gatsby-theme-catalyst-core/src/contexts/mobile-context.js
@@ -5,21 +5,18 @@ export const MobileContext = createContext()
 
 export const MobileProvider = ({ children }) => {
   const [isMobile, setIsMobile] = useState(true)
-  const isClient = typeof window === "object"
   const { mobileMenuBreakpoint } = useCatalystConfig()
 
   useEffect(
     () => {
-      if (isClient) {
-        const mq = window.matchMedia("(min-width:" + mobileMenuBreakpoint + ")")
-        const getValue = () => {
-          return !mq.matches
-        }
-        setIsMobile(getValue)
-        const handler = () => setIsMobile(getValue)
-        mq.addListener(handler)
-        return () => mq.removeListener(handler)
+      const mq = window.matchMedia("(min-width:" + mobileMenuBreakpoint + ")")
+      const getValue = () => {
+        return !mq.matches
       }
+      setIsMobile(getValue)
+      const handler = () => setIsMobile(getValue)
+      mq.addListener(handler)
+      return () => mq.removeListener(handler)
     }, // eslint-disable-next-line react-hooks/exhaustive-deps
     [] // Empty array ensures effect is only run on mount and unmount
   )
