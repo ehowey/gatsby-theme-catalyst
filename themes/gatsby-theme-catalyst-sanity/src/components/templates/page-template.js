6212e8173808236c20fed1500548760401f09dfa
Eric Howey <ehowey@gmail.com>
2020-03-21 08:42:21 -0600

    Beginning work on theme-ui and Sanity integration

--- themes/gatsby-theme-catalyst-sanity/src/components/templates/page-template.js
+++ themes/gatsby-theme-catalyst-sanity/src/components/templates/page-template.js
@@ -1,16 +1,20 @@
 /** @jsx jsx */
 import { jsx, Styled } from "theme-ui"
 import { SEO, Layout } from "gatsby-theme-catalyst-core"
+import { ThemeProvider } from "theme-ui"
 import SanityContent from "../sanity/sanity-content"
+import theme from "../sanity/theme.js"
 
 const PageTemplate = ({ data }) => {
   const page = data.sanityPage
   return (
-    <Layout>
-      <SEO title={page.title} />
-      <Styled.h1>{page.title}</Styled.h1>
-      <SanityContent data={page._rawBody} />
-    </Layout>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <SEO title={page.title} />
+        <Styled.h1>{page.title}</Styled.h1>
+        <SanityContent data={page._rawBody} />
+      </Layout>
+    </ThemeProvider>
   )
 }
 
