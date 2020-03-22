3ce6974a71380653a06ae2d8fd8a10bcd25fe9e0
Eric Howey <ehowey@gmail.com>
2020-03-21 20:28:40 -0600

    Updated versions

---
+++ themes/gatsby-theme-catalyst-sanity/src/components/sanity/theme.js
@@ -0,0 +1,26 @@
+// See https://theme-ui.com/ for more info and also https://www.gatsbyjs.org/docs/theme-ui/
+// Try changing some of the colors below to see what happens.
+
+export default {
+  colors: {
+    primary: "green",
+    secondary: "blue",
+    accent: "red",
+    background: "pink",
+    header: {
+      background: "orange",
+      backgroundOpen: baseColors.red[1],
+      text: baseColors.gray[8],
+      textOpen: baseColors.gray[8],
+      icons: baseColors.gray[6],
+      iconsHover: baseColors.red[8],
+      iconsOpen: baseColors.gray[6],
+    },
+    footer: {
+      background: baseColors.gray[2],
+      text: baseColors.gray[8],
+      links: baseColors.gray[8],
+      icons: baseColors.gray[8],
+    },
+  },
+}
