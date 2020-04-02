1054ff5e9c41fed05d9278174356931ab279f74b
Eric Howey <ehowey@gmail.com>
2020-04-02 16:36:19 -0600

    Updated comments for theme file

--- starters/gatsby-starter-catalyst/src/gatsby-plugin-theme-ui/index.js
+++ starters/gatsby-starter-catalyst/src/gatsby-plugin-theme-ui/index.js
@@ -2,7 +2,15 @@ import { merge } from "theme-ui"
 import { BaseTheme } from "gatsby-theme-catalyst-core"
 
 export default merge(BaseTheme, {
-  // Modifications to the base theme go here.
+  // Modifications to the base theme go here. This is an example changing colors and using variants to change your navigation links. Uncomment the code below to see what happens.
+  // colors: {
+  //   background: "pink",
+  //   modes: {
+  //     dark: {
+  //       background: "purple",
+  //     },
+  //   },
+  // },
   // variants: {
   //   navLink: {
   //     "::after": {
