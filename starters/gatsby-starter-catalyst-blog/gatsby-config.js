4e39347748686cbf19369db1be6928367d1147f5
Eric Howey <ehowey@gmail.com>
2020-04-10 21:20:45 -0600

    Revert gatsby-config in blog starter

--- starters/gatsby-starter-catalyst-blog/gatsby-config.js
+++ starters/gatsby-starter-catalyst-blog/gatsby-config.js
@@ -15,23 +15,6 @@ module.exports = {
         name: `Page 2`,
         link: `/page-2`,
         type: `internal`, //internal or anchor
-        subMenu: [
-          {
-            name: `Sub-1`,
-            link: `/sub`,
-            type: `internal`,
-          },
-          {
-            name: `Sub-2-Longer`,
-            link: `/sub`,
-            type: `internal`,
-          },
-          {
-            name: `Sub-1-Even-Longer-Still`,
-            link: `/sub`,
-            type: `internal`,
-          },
-        ],
       },
       {
         name: `Blog`,
