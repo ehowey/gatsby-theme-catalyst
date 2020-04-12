c9bcc638bf1ba3f2b18fb52a078e9b1cf97bf31e
Eric Howey <ehowey@gmail.com>
2020-04-11 22:34:50 -0600

    Remove unused isNavOpen

--- themes/gatsby-theme-catalyst-header-side/src/components/navbar/nav-ul-submenu.js
+++ themes/gatsby-theme-catalyst-header-side/src/components/navbar/nav-ul-submenu.js
@@ -1,11 +1,7 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { useContext } from "react"
-import { NavContext } from "gatsby-theme-catalyst-core"
 
 const NavUlDropdown = ({ children }) => {
-  const [isNavOpen] = useContext(NavContext)
-
   return (
     <ul
       sx={{
