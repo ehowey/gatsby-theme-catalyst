3932df56a7042a299ba23c06355c16064718e034
Eric Howey <ehowey@gmail.com>
2020-05-26 23:40:17 -0600

    Updated tests

--- cypress/e2e/docs.test.js
+++ cypress/e2e/docs.test.js
@@ -1,6 +1,6 @@
 describe("Smoke test", () => {
   it("Site loads", () => {
-    cy.visit(`/`).assertRoute(`/`)
+    cy.visit(`http://localhost:9000/`).assertRoute(`http://localhost:9000/`)
     cy.get("header").should("be.visible")
     cy.get("main").should("be.visible")
     cy.get("footer").should("be.visible")
--- cypress/old-tests/smoke.test.js
+++
@@ -1,8 +0,0 @@
-describe("Smoke test", () => {
-  it("Site loads", () => {
-    cy.visit(`/`).assertRoute(`/`)
-    cy.get("header").should("be.visible")
-    cy.get("main").should("be.visible")
-    cy.get("footer").should("be.visible")
-  })
-})
