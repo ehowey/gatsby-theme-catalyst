c1430f66aa7f408161d5755eb077fcbc198bb2f8
Eric Howey <ehowey@gmail.com>
2020-05-27 10:01:04 -0600

    (all): Fixing broken tests (#637)
    * Updated tests
    
    * Updated scripts

---
+++ cypress/e2e/themes.test.js
@@ -0,0 +1,25 @@
+describe("Smoke test", () => {
+  it("Site loads", () => {
+    cy.visit(`/`).assertRoute(`/`)
+    cy.viewport(1440, 900)
+    cy.get("header").should("be.visible")
+    cy.get("main").should("be.visible")
+    cy.get("footer").should("be.visible")
+  })
+})
+describe("Navigation Menu Tests", () => {
+  it("Navigation Works", () => {
+    cy.viewport(1440, 900)
+    cy.get("header").then(($header) => {
+      if ($header.find("nav").length) {
+        cy.get("nav").should("be.visible")
+        cy.viewport(375, 812)
+        cy.get("nav").should("not.be.visible")
+        cy.get('button[aria-label*="Toggle Menu"]').should("be.visible").click()
+        cy.get("nav").find("a").should("be.visible")
+      } else {
+        return "No Nav"
+      }
+    })
+  })
+})
