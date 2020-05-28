4981dfa41132c6b4c5d8fda49cbf553063fc1ece
Eric Howey <ehowey@gmail.com>
2020-05-27 21:03:45 -0600

    Test for color mode toggle

--- cypress/e2e/themes.test.js
+++ cypress/e2e/themes.test.js
@@ -1,4 +1,4 @@
-describe("Smoke test", () => {
+describe("Smoke Test", () => {
   it("Site loads", () => {
     cy.visit(`/`).assertRoute(`/`)
     cy.viewport(1440, 900)
@@ -7,7 +7,7 @@ describe("Smoke test", () => {
     cy.get("footer").should("be.visible")
   })
 })
-describe("Navigation Menu Tests", () => {
+describe("Navigation Menu Test", () => {
   it("Navigation Works", () => {
     cy.viewport(1440, 900)
     cy.get("header").then(($header) => {
@@ -17,8 +17,28 @@ describe("Navigation Menu Tests", () => {
         cy.get("nav").should("not.be.visible")
         cy.get('button[aria-label*="Toggle Menu"]').should("be.visible").click()
         cy.get("nav").find("a").should("be.visible")
+        cy.get('button[aria-label*="Toggle Menu"]').click()
       } else {
         return "No Nav"
+      }
+    })
+  })
+})
+describe("Color Mode Toggle Test", () => {
+  it("Toggle works", () => {
+    cy.viewport(1440, 900)
+    cy.get("header").then(($nav) => {
+      if ($nav.find('button[aria-label*="Toggle dark mode"]').length) {
+        cy.get('button[aria-label*="Toggle dark mode"]').click()
+        cy.get("body").should("have.css", "background-color", "rgb(26, 32, 44)")
+        cy.get('button[aria-label*="Toggle dark mode"]').click()
+        cy.get("body").should(
+          "have.css",
+          "background-color",
+          "rgb(247, 250, 252)"
+        )
+      } else {
+        return "No color mode toggle"
       }
     })
   })
