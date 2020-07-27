7f3d53280ec8f3892237a9faac0c2419b5acd231
Eric Howey <ehowey@gmail.com>
2020-07-27 07:31:10 -0600

    (core): remove default dark mode from base theme-ui spec (#814)
    * Move colors from baseTheme to starters
    
    * Update docs
    
    * Add colors back into helium starter
    
    * Update docs
    
    Co-authored-by: Eric Howey <erichowey@Erics-MacBook-Air.local>

--- themes/gatsby-theme-catalyst-core/src/gatsby-plugin-theme-ui/index.js
+++ themes/gatsby-theme-catalyst-core/src/gatsby-plugin-theme-ui/index.js
@@ -40,33 +40,6 @@ export default {
       links: baseColors.gray[8],
       icons: baseColors.gray[8],
     },
-    // You can delete dark mode by removing the "mode" object and/or setting useColorMode to false in gatsby-theme-catalyst-core
-    modes: {
-      dark: {
-        background: baseColors.gray[9],
-        text: baseColors.gray[1],
-        textGray: "#9f9f9f",
-        primary: "#458ad2",
-        secondary: baseColors.orange[7],
-        accent: baseColors.gray[8],
-        highlight: baseColors.orange[5],
-        muted: baseColors.gray[8],
-        header: {
-          text: baseColors.gray[1],
-          textOpen: baseColors.gray[1],
-          background: "#232946",
-          backgroundOpen: baseColors.gray[8],
-          icons: baseColors.gray[1],
-          iconsOpen: baseColors.gray[1],
-        },
-        footer: {
-          background: "#232946",
-          text: baseColors.gray[1],
-          links: baseColors.gray[1],
-          icons: baseColors.gray[1],
-        },
-      },
-    },
   },
   sizes: {
     ...tailwind.sizes,
