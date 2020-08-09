5f8e2f007f6ee774ea04f424df9ad6e1d27c485b
Eric Howey <erichowey@Erics-MacBook-Air.local>
2020-08-09 16:43:03 -0600

    Fixed links

--- themes/gatsby-theme-catalyst-sanity/src/components/templates/category-template.js
+++ themes/gatsby-theme-catalyst-sanity/src/components/templates/category-template.js
@@ -2,8 +2,11 @@
 import { jsx, Styled } from "theme-ui"
 import { Link } from "gatsby"
 import { SEO, Layout } from "gatsby-theme-catalyst-core"
+import { useSanityConfig } from "gatsby-theme-catalyst-sanity"
 
 const CategoryPage = ({ posts, category }) => {
+  const { sanityPostPath } = useSanityConfig()
+  const rootPath = sanityPostPath.replace(/\/*$/, `/`) //Ensure trailing slash
   return (
     <Layout>
       <SEO title={"Category: " + category} />
@@ -12,7 +15,11 @@ const CategoryPage = ({ posts, category }) => {
         {posts.map((post) => (
           <Styled.li key={post.slug}>
             <Styled.p>
-              <Styled.a as={Link} to={post.slug.current} sx={{ fontSize: 3 }}>
+              <Styled.a
+                as={Link}
+                to={rootPath.concat(post.slug.current.replace(/\/*$/, `/`))}
+                sx={{ fontSize: 3 }}
+              >
                 {post.title}
               </Styled.a>
               <br />
