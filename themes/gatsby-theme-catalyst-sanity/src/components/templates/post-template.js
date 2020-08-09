09539dff0a372372106562f2a9e7bc784e08a420
Eric Howey <erichowey@Erics-MacBook-Air.local>
2020-08-09 09:22:57 -0600

    Update category styling

--- themes/gatsby-theme-catalyst-sanity/src/components/templates/post-template.js
+++ themes/gatsby-theme-catalyst-sanity/src/components/templates/post-template.js
@@ -15,29 +15,32 @@ const PostTemplate = ({ data }) => {
         <SEO title={post.title} />
         <Styled.h1>{post.title}</Styled.h1>
         <Styled.p>{post.date}</Styled.p>
-        <Styled.ul
-          sx={{
-            listStyle: "none",
-            m: 0,
-            p: 0,
-            display: "flex",
-          }}
-        >
-          {post.categories.map((category) => (
-            <Styled.li
-              sx={{
-                mr: 2,
-                ":last-of-type": {
-                  mr: 0,
-                },
-              }}
-            >
-              <Styled.a as={Link} to={`/categories/${category.slug.current}`}>
-                {category.title}
-              </Styled.a>
-            </Styled.li>
-          ))}
-        </Styled.ul>
+        <Styled.p>
+          Categories:
+          <Styled.ul
+            sx={{
+              listStyle: "none",
+              m: 0,
+              p: 0,
+              display: "flex",
+            }}
+          >
+            {post.categories.map((category) => (
+              <Styled.li
+                sx={{
+                  mr: 2,
+                  ":last-of-type": {
+                    mr: 0,
+                  },
+                }}
+              >
+                <Styled.a as={Link} to={`/categories/${category.slug.current}`}>
+                  {category.title}
+                </Styled.a>
+              </Styled.li>
+            ))}
+          </Styled.ul>
+        </Styled.p>
         <SanityContent data={post._rawBody} />
       </Layout>
     </SanityThemeProvider>
