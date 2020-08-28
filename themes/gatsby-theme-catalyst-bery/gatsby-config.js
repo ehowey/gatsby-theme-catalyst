ec50824e778275327509f8130c16cf4bf012d6c0
Eric Howey <ehowey@gmail.com>
2020-08-27 21:38:38 -0600

    (bery): add rss feed (#874)
    * WIP on adding a feed
    
    * Finished RSS feed
    
    Co-authored-by: Eric Howey <erichowey@Erics-MacBook-Air.local>

--- themes/gatsby-theme-catalyst-bery/gatsby-config.js
+++ themes/gatsby-theme-catalyst-bery/gatsby-config.js
@@ -45,6 +45,89 @@ module.exports = (themeOptions) => {
             themeOptions.sanityDisplayProjectListTitle,
         },
       },
+      {
+        resolve: `gatsby-plugin-feed`,
+        options: {
+          query: `
+            {
+              site {
+                siteMetadata {
+                  title
+                  description
+                  siteUrl
+                  site_url: siteUrl
+                }
+              }
+            }
+          `,
+          feeds: [
+            {
+              output: `/rss.xml`,
+              title: themeOptions.rssTitle || `RSS Feed`,
+              description: themeOptions.rssDescription || `A great RSS feed`,
+              query: `
+                {
+                  allSanityPost(
+                    sort: { fields: [date, title], order: DESC }
+                    limit: 1000
+                  ) {
+                    nodes {
+                      id
+                      date(formatString: "ddd, DD MMM YYYY HH:mm:ss ZZ")
+                      title
+                      slug {
+                        current
+                      }
+                      author {
+                        name
+                      }
+                      featuredImage {
+                        asset {
+                          url
+                        }
+                      }
+                      excerpt(limit: 280)
+                    }
+                  }
+                  catalystSanityConfig {
+                    sanityPostPath
+                  }
+                }
+              `,
+              serialize: ({
+                query: {
+                  site: {
+                    siteMetadata: { siteUrl },
+                  },
+                  catalystSanityConfig,
+                  allSanityPost,
+                },
+              }) => {
+                const rssFeed = allSanityPost.nodes.map((node) => {
+                  const rssImage = node.featuredImage.asset.url
+                  const rootPath = siteUrl
+                    .concat(catalystSanityConfig.sanityPostPath)
+                    .replace(/\/*$/, `/`)
+                  const rssUrl = rootPath
+                    .concat(node.slug.current)
+                    .replace(/\/*$/, `/`)
+                  const serialized = {
+                    guid: rssUrl,
+                    url: rssUrl,
+                    title: node.title,
+                    author: node.author.name,
+                    description: node.excerpt,
+                    pubDate: node.date,
+                    enclosure: { url: rssImage },
+                  }
+                  return serialized
+                })
+                return rssFeed
+              },
+            },
+          ],
+        },
+      },
     ].filter(Boolean),
   }
 }
