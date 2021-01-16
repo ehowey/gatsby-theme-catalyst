3a433d5fd80cae1959a96cedad66994283d41bde
ehowey <ehowey@gmail.com>
2021-01-15 20:56:05 -0700

    Initial theme and starter

---
+++ starters/gatsby-starter-catalyst-ecommerce/gatsby-config.js
@@ -0,0 +1,57 @@
+require("dotenv").config()
+
+module.exports = {
+  siteMetadata: {
+    title: `Placeholder title`, // Sourced from SANITY
+    description: `Placeholder description`, // Sourced from SANITY
+    keywords: [`keyword 1`, `keyword 2`, `keyword 3`], // Sourced from SANITY
+    author: `Placeholder author`, // Sourced from SANITY
+    siteUrl: `https://gatsby-starter-catalyst-ecommerce.netlify.app`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
+    menuLinks: [
+      // Sourced from SANITY
+      {
+        name: `Page 1`,
+        link: `/page-1`,
+        type: `internal`, //internal or anchor
+      },
+    ],
+    socialLinks: [
+      // Sourced from SANITY
+      {
+        name: `Placeholder`,
+        link: `eric@erichowey.dev`,
+        location: `all`, //Options are "all", "header", "footer"
+      },
+    ],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-catalyst-ecommerce`,
+      options: {
+        // Example for an env variable
+        // sanityProjectID: process.env.SANITY_PROJECT_ID,
+        // sanityDataset: process.env.SANITY_DATASET
+        //
+        // Default values
+        // sanityProjectId: REQUIRED,
+        // sanityDataset: "production",
+        // sanityToken: null,
+        // sanityWatchMode: true,
+        // sanityOverlayDrafts: false, // Requires a token and private dataset
+        sanityProjectId: `pyvaull7`, // Required
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-catalyst-hydrogen`,
+        short_name: `catalyst`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#cccccc`,
+        display: `minimal-ui`,
+        icon: `content/assets/catalyst-site-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
---
+++ themes/gatsby-theme-catalyst-ecommerce/gatsby-node.js
@@ -0,0 +1,31 @@
+// // Create Home Page
+// async function createSanityHomePage(actions, reporter) {
+//   const { createPage } = actions
+
+//   reporter.info(`Creating home page`)
+
+//   createPage({
+//     path: "/",
+//     component: require.resolve("./src/components/home-page/home-query"),
+//     context: {},
+//   })
+// }
+
+// // Create Work Page
+// async function createSanityWorkPage(actions, reporter) {
+//   const { createPage } = actions
+
+//   reporter.info(`Creating work page`)
+
+//   createPage({
+//     path: "/work/",
+//     component: require.resolve("./src/components/work-page/work-query"),
+//     context: {},
+//   })
+// }
+
+// // Create all the pages
+// exports.createPages = async ({ actions, reporter }) => {
+//   await createSanityHomePage(actions, reporter)
+//   await createSanityWorkPage(actions, reporter)
+// }
