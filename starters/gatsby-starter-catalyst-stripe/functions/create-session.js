de862d21e0cb617ac51adad1c59a59b553809e97
ehowey <ehowey@gmail.com>
2021-02-11 21:54:04 -0700

    Server side validation of stock and sale price

--- starters/gatsby-starter-catalyst-stripe/functions/create-session.js
+++ starters/gatsby-starter-catalyst-stripe/functions/create-session.js
@@ -9,7 +9,6 @@ const stripe = require("stripe")(process.env.GATSBY_STRIPE_SECRET_KEY)
 const sanityClient = require("@sanity/client")
 const dollarsToCents = require("dollars-to-cents")
 const stripeConfig = require("../stripe-config")
-
 const validateCartItems = require("use-shopping-cart/src/serverUtil")
   .validateCartItems
 
@@ -24,14 +23,13 @@ const client = sanityClient({
 exports.handler = async (event) => {
   try {
     const cartItemsFromWeb = JSON.parse(event.body)
+    const arrayFromWeb = Object.values(cartItemsFromWeb)
     const productIdsFromWeb = Object.keys(cartItemsFromWeb)
     const idsForQuery = productIdsFromWeb.map((id) => "'" + id + "'")
 
-    // const query = `*[_type == "productVariant" && product_id in [${idsForQuery}]]{"image": images[0].asset->url, ...}`
     const query = `*[_type == "product" && references(*[_type=="productVariant" && product_id in [${idsForQuery}]]._id)]{..., "purchasedVariants": *[_type == "productVariant" && product_id in [${idsForQuery}]]}`
     const params = {}
     const data = await client.fetch(query, params)
-    console.log(data)
     const productsFromSanity = await Promise.all(
       data
         .map((product) => {
@@ -47,6 +45,7 @@ exports.handler = async (event) => {
               id: variant.product_id,
               price: onSale ? salePrice : dollarsToCents(variant.price),
               currency: stripeConfig.currency,
+              stock: variant.stock,
               // image: variant.image,
               product_data: {
                 metadata: {
@@ -63,6 +62,29 @@ exports.handler = async (event) => {
         .flat()
     )
 
+    const checkStock = (sanityProduct, webProduct) => {
+      let stockStatus = []
+      webProduct.forEach((element) => {
+        const webId = element.id
+        const webQuantity = element.quantity
+        sanityProduct.forEach((element) => {
+          if (element.id === webId && webQuantity <= element.stock) {
+            stockStatus.push(true)
+          }
+        })
+      })
+      const inStock =
+        stockStatus.length > 0 && stockStatus.every((v) => v === true)
+      return inStock
+    }
+
+    const isStocked = checkStock(productsFromSanity, arrayFromWeb)
+
+    if (!isStocked)
+      throw new Error(
+        `Attempted to buy a quantity that exceeded available stock.`
+      )
+
     const line_items = validateCartItems(productsFromSanity, cartItemsFromWeb)
 
     const session = await stripe.checkout.sessions.create({
@@ -74,9 +96,6 @@ exports.handler = async (event) => {
       mode: stripeConfig.mode,
       success_url: stripeConfig.successUrl,
       cancel_url: stripeConfig.cancelUrl,
-      metadata: {
-        id: "123abc",
-      },
       line_items,
     })
 
