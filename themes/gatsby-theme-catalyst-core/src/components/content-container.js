41cae2f73f30d6b6c95b45be423055ea3ef12379
ehowey <ehowey@gmail.com>
2021-03-23 19:36:28 -0600

    Remove full bleed

--- themes/gatsby-theme-catalyst-core/src/components/content-container.js
+++ themes/gatsby-theme-catalyst-core/src/components/content-container.js
@@ -1,16 +1,14 @@
 /** @jsx jsx */
-import { jsx, useThemeUI } from "theme-ui"
+import { jsx } from "theme-ui"
 
 const ContentContainer = ({ children }) => {
-  const { theme } = useThemeUI()
-  const contentWidth = theme.sizes.maxContentWidth
   return (
     <div
       sx={{
-        display: "grid",
-        gridTemplateColumns: `minmax(0, 1fr)  min(${contentWidth}, calc(100% - 32px)) minmax(0, 1fr)`,
-        "> *": { gridColumn: 2 },
-        gridGap: "16px",
+        maxWidth: "maxContentWidth",
+        my: 0,
+        mx: "auto",
+        px: 3,
         variant: "variants.contentContainer",
       }}
     >
