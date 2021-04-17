ebae06139f4ef686176b2122a38371d43980c57d
ehowey <ehowey@gmail.com>
2021-04-16 20:48:16 -0600

    Update color mode button and tests

--- cypress/e2e/basic.test.js
+++ cypress/e2e/basic.test.js
@@ -60,3 +60,30 @@ describe("Color Mode Toggle Test", () => {
     })
   })
 })
+
+describe("Markdown test", () => {
+  it("Renders h1", () => {
+    cy.get("h1").contains("Gatsby Starter Catalyst Core")
+  })
+  it("Renders link", () => {
+    cy.get("main").within(() => {
+      cy.get("a").should("have.css", "color", "rgb(43, 108, 176)")
+    })
+  })
+  it("Renders ol", () => {
+    cy.get("main").within(() => {
+      cy.get("ol").contains("li", "Bird")
+    })
+  })
+  it("Renders ul", () => {
+    cy.get("main").within(() => {
+      cy.get("ul").contains("li", "Red")
+    })
+  })
+  it("Renders image", () => {
+    cy.get("main")
+      .find("img")
+      .should("have.attr", "src")
+      .should("include", "image1")
+  })
+})
